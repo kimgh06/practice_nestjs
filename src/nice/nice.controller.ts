5785bee29708f224ded4a8ea1603713a6b307258
kimgh06 <kkh061101@naver.com>
2024-04-10 21:54:43 +0900

    crud

--- src/nice/nice.controller.ts
+++ src/nice/nice.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('nice')
 export class NiceController {
   @Get(':id')
   hello(@Param("id") id: string): string {
     return id
+  }
+  @Post()
+  creates(): string {
+    return "sdfd"
+  }
+  @Put(":id")
+  asdf(@Param("id") id: string): string {
+    return id + "puts"
+  }
+  @Delete()
+  asdfasdf(): string {
+    return 'del'
+  }
+  @Patch()
+  asdasdff(): string {
+    return 'patch'
   }
 }
