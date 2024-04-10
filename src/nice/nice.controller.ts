e111f87e067c55d27b81acfcdad7a18e4ef41f20
kimgh06 <kkh061101@naver.com>
2024-04-10 22:28:21 +0900

    querystring and parameters

--- src/nice/nice.controller.ts
+++ src/nice/nice.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('nice')
 export class NiceController {
+  @Get("/search")
+  search(@Query("id") id: string): string {
+    return id
+  }
   @Get(':id')
   hello(@Param("id") id: string): string {
     return id
   }
   @Post()
-  creates(): string {
-    return "sdfd"
+  creates(@Body() movie: object): object {
+    console.log(movie)
+    return movie
   }
   @Put(":id")
-  asdf(@Param("id") id: string): string {
-    return id + "puts"
+  asdf(@Param("id") id: string, @Body() body: object): object {
+    return {
+      fxxk: id,
+      ...body
+    }
   }
   @Delete()
   asdfasdf(): string {
