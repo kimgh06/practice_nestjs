a5524dd512f8b8c24ff961359a1341acf6705d29
kimgh06 <kkh061101@naver.com>
2024-04-10 21:13:53 +0900

    diff port

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration from 'config/configuration';
 
 @Module({
   imports: [ConfigModule.forRoot({
     cache: true,
     isGlobal: true,
-    load: [configuration]
   })],
   controllers: [AppController],
   providers: [AppService],
