3c3e195973b3b9ede97eb2af85fa1bbc5e59e2c9
kimgh06 <kkh061101@naver.com>
2024-04-15 08:29:24 +0900

    tobegreaterthan()

--- src/nice/nice.service.spec.ts
+++ src/nice/nice.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NiceService } from './nice.service';
 import { NiceController } from './nice.controller';
-import { Nice } from './entities/nice.entity';
 import { NotFoundException } from '@nestjs/common';
 
 describe('NiceService', () => {
@@ -47,6 +46,40 @@ describe('NiceService', () => {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual(`not found the id: ${9999}`);
       }
+    })
+  })
+
+  describe('delon', () => {
+    it('delets one', () => {
+      service.create({
+        "title": "helloworL",
+        "year": 1324,
+        "genres": ["asdfasdf", "fefe"]
+      });
+      const before = service.getall();
+      service.delone(1);
+      const after = service.getall();
+      expect(after.length).toBeLessThan(before.length);
+    })
+    it('not found', () => {
+      try {
+        service.delone(-1);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    })
+  })
+
+  describe('create', () => {
+    it('create one', () => {
+      const before = service.getall().length;
+      service.create({
+        "title": "helloworL",
+        "year": 1324,
+        "genres": ["asdfasdf", "fefe"]
+      });
+      const after = service.getall().length;
+      expect(after).toBeGreaterThan(before);
     })
   })
 });
