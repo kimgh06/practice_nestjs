177dad8735e0da71118b51ab582e8d76a1ca87d9
kimgh06 <kkh061101@naver.com>
2024-04-16 08:08:14 +0900

    e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpServer, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let server: any;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }));
+    server = request(app.getHttpServer());
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  describe('/nice', () => {
+    it('/ (GET)', () => {
+      return server
+        .get('/nice')
+        .expect(200)
+        .expect([]);
+    });
+    it('/ (POST)', () => {
+      return server
+        .post('/nice')
+        .send({
+          "title": "helloworL",
+          "year": 1324,
+          "genres": ["asdfasdf", "fefe"]
+        })
+        .expect(201)
+    });
+  })
 });
